27b43b5c6bab0a29b7b957e21ee87b024b0e49dc
ximet <dq.ximet@gmail.com>
2017-02-05 13:16:55 +0300

    strange urho

--- ARSample/MainActivity.cs
+++ ARSample/MainActivity.cs
@@ -1,27 +1,68 @@
 ﻿using Android.App;
 using Android.Widget;
 using Android.OS;
+using Urho;
+using Urho.Actions;
+using Urho.Shapes;
+using Android.Content.PM;
+using Urho.Droid;
+using Android.Views;
 
 namespace ARSample
 {
-	[Activity(Label = "ARSample", MainLauncher = true, Icon = "@mipmap/icon")]
+	[Activity(Label = "ARSampleZ", MainLauncher = true,
+	Icon = "@mipmap/icon", Theme = "@android:style/Theme.NoTitleBar.Fullscreen",
+	ConfigurationChanges = ConfigChanges.KeyboardHidden | ConfigChanges.Orientation,
+	ScreenOrientation = ScreenOrientation.Portrait)]
 	public class MainActivity : Activity
 	{
-		int count = 1;
-
-		protected override void OnCreate(Bundle savedInstanceState)
+		protected override void OnCreate(Bundle bundle)
 		{
-			base.OnCreate(savedInstanceState);
+			base.OnCreate(bundle);
+			var mLayout = new AbsoluteLayout(this);
+			var surface = UrhoSurface.CreateSurface<ARSample>(this);
+			mLayout.AddView(surface);
+			SetContentView(mLayout);
+		}
 
-			// Set our view from the "main" layout resource
-			SetContentView(Resource.Layout.Main);
+		protected override void OnResume()
+		{
+			UrhoSurface.OnResume();
+			base.OnResume();
+		}
 
-			// Get our button from the layout resource,
-			// and attach an event to it
-			Button button = FindViewById<Button>(Resource.Id.myButton);
+		protected override void OnPause()
+		{
+			UrhoSurface.OnPause();
+			base.OnPause();
+		}
 
-			button.Click += delegate { button.Text = string.Format("{0} clicks!", count++); };
+		public override void OnLowMemory()
+		{
+			UrhoSurface.OnLowMemory();
+			base.OnLowMemory();
+		}
+
+		protected override void OnDestroy()
+		{
+			UrhoSurface.OnDestroy();
+			base.OnDestroy();
+		}
+
+		public override bool DispatchKeyEvent(KeyEvent e)
+		{
+			if (!UrhoSurface.DispatchKeyEvent(e))
+				return false;
+			return base.DispatchKeyEvent(e);
+		}
+
+		public override void OnWindowFocusChanged(bool hasFocus)
+		{
+			UrhoSurface.OnWindowFocusChanged(hasFocus);
+			base.OnWindowFocusChanged(hasFocus);
 		}
 	}
+
 }
+
 
